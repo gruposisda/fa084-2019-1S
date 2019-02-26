e42fac7b9eb682568b26cfbb4c5dd826f19bea9d
mferrac <matheus.ferraciolli@gmail.com>
2019-02-26 10:14:48 -0300

    finished sktech

--- 01_introducao/code/01_introducao.R
+++ 01_introducao/code/01_introducao.R
@@ -9,7 +9,7 @@
 
 # Criar variáveis e ambientes
 
-# 2. Tipos de variáveis
+# 1. Tipos de variáveis -----
 # numeric, character e boolean
 # https://swcarpentry.github.io/r-novice-inflammation/13-supp-data-structures/
 a = 1
@@ -31,7 +31,7 @@ class(d)
 a + d
 
 
-# 2. Vetores e dataframes
+# 2. Vetores -----
 #Vetores armazenam valores DO MESMO TIPO e são definidos com a função c().
 (2,1)
 c(2,1)
@@ -60,7 +60,17 @@ numeros[1] #elemento 1 do vetor numeros (ou seja , o numero 5)
 letras[2] #elemento 2 do vetor letras (ou seja , o caractere "B")
 chars[10] # se nao existe, retorna na
 
-#Dataframes
+#se quisermos mais de um elemento:
+
+vec = letters
+vec[4:10]
+
+#subset logico
+vec == 'a'
+vec == 'c'
+vec[(vec == 'c' | vec == 'd')]
+
+# 3. Dataframes -----
 #tipo de estrutura mais comum entre os dados, a classica tabela
 #pode-se pensar em um dataframe como muitos vetores um ao lado do outro e todos do mesmo tamanho
 numeros
@@ -79,29 +89,81 @@ df[,2]
 df[,1]
 
 
-
-# Explorar conjunto de dados:
+# Explorar conjunto de dados 
 iris
 df = iris
 class(df)
 
-#principais funcoes head, tail, dim, nrow, ncol, str, colnames
+#subset do iris
+df[50:100,]
+
+#principais funcoes head, tail, dim, nrow, ncol, str, colnames 
 head(df)
 tail(df)
 dim(df)
 colnames(df)
 str(df)
 
-#histograma, plots, media, desvio padrao, boxplot
+# 4. Histograma, plots, boxplot -----
+df = iris
+dados = df[,1]
+hist(dados)
+hist(dados,breaks = 20)
+hist(dados,breaks = 30, col='blue')
+hist(dados,breaks = 30, col='blue',main='Histograma de Sepal Length')
+hist(dados,breaks = 30, col='blue',main='Histograma de Sepal Length',
+     xlab = 'Sepal Length', ylab= 'Frequência')
 
-df = mtcars
-head(df)
-tail(df)
-dim(df)
+#plot de densidade
+dados_dens = density(df[,1])
+
+plot(dados_dens,main='Histograma de Sepal Length',
+     xlab = 'Sepal Length', ylab= 'Frequência',col='red')
+lines(density(df[,3]))
+
+
+#plot x vs y
+df = cars
+help(cars)
 colnames(df)
-str(df)
+plot(df$speed,df$dist,xlab ='Speed (mph) ',ylab='Distance to stop (ft)')
+reg = lm(dist~speed,data = df)
+abline(reg,col='blue')
 
-# 3. Estrutura de diretórios: Começamos colocando os arquivos em uma pasta chamada `code` (ou `codigos`) e os dados em outra, chamada `data` (ou `dados`).
-# Evite acentos e espaços em nomes de pastas e arquivos.
+#boxplots
+boxplot(iris[,c(1,2,3,4)],col=terrain.colors(4),main = 'Boxplot Iris')
+1:4
+
+
+# 5. Big dataset -----
+#https://www.kaggle.com/tsiaras/uk-road-safety-accidents-and-vehicles
+t0 = Sys.time()
+bigdf = read.csv('./data/uk-road-safety-accidents-and-vehicles/Accident_Information.csv')
+t1 = Sys.time()
+t1-t0
+dim(bigdf)
+colnames(bigdf)
+
+road_cond_acc_sev = table(bigdf$Road_Surface_Conditions,bigdf$Accident_Severity)
+road_cond_acc_sev
+dat = as.data.frame(prop.table(road_cond_acc_sev,margin = 1)[2:6,])
+
+
+library(ggplot2)
+ggplot(dat, aes(x=Var1,y=Freq,fill=Var2)) + 
+  geom_bar(stat='identity', position = 'dodge', alpha = 2/3)
+
+ggplot(dat[dat$Var2!='Slight',], aes(x=Var1,y=Freq,fill=Var2)) + 
+  geom_bar(stat='identity', position = 'dodge', alpha = 2/3)
+
+speed_lim_acc_sev = table(bigdf$Speed_limit,bigdf$Accident_Severity)
+speed_lim_acc_sev
+as.data.frame(prop.table(speed_lim_acc_sev,margin = 1))
+
+
+
+
+# 6. Estrutura de diretórios -----
+
 
 
