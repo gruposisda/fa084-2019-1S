5652030193214951a384d26c137d656032694147
mferrac <matheus.ferraciolli@gmail.com>
2019-03-12 21:55:59 -0300

    progress on lesson 3

--- 03_linear_regression/03_linear_regression.R
+++ 03_linear_regression/03_linear_regression.R
@@ -1,4 +1,120 @@
-"Least Squares Estimation. (Slides for this and other Data Science courses may be found at github
-                           | https://github.com/DataScienceSpecialization/courses. If you care to use them, they must be downloaded as a zip file and viewed
-                           | locally. This lesson corresponds to Regression_Models/01_03_ols. Galton data is from John Verzani's website,
-| http://wiener.math.csi.cuny.edu/UsingR/)"
+#visualizacao/explicacao do conjunto de dados
+library(ggplot2)
+library(readxl)
+
+#df vs tibble
+df = read.csv('~/data/fa084/03_regressao_linear/data/fa084_tch_mult.csv')
+tib = read_xlsx('~/data/fa084/03_regressao_linear/data/fa084_dados_cana_acucar.xlsx')
+dim(df)
+head(df)
+colnames(df)
+
+#df = select(df,contains('_i'),estagio,variedade, solo, espac, tch)
+#write.csv(df,'~/data/fa084/03_regressao_linear/data/fa084_tch_mult.csv',row.names =F)
+
+#remover features
+cols_to_remove_name = c('variedade', 'solo','espac')
+cols_to_remove_number = match(cols_to_remove_name,colnames(df))
+df = df[,-cols_to_remove_number]
+
+#explorar features
+df %>% select(contains('ppt')) %>% gather('ppt','mm_chuva') %>% 
+  ggplot(aes(mm_chuva,color=ppt)) + geom_density(size = 1.5) + theme_classic()+
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold")) + ylab('')
+
+df %>% select(contains('lst')) %>% gather('temp','Kelvin') %>% 
+  ggplot(aes(Kelvin,color=temp)) + geom_density(size = 1.5) + theme_classic()+
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold")) + ylab('')
+
+#construir features #deixar eles fazerem
+#total de ppt do periodo todo
+colnames(df)
+df$ppt_total = df$ppt_i + df$ppt_ii + df$ppt_iii
+
+#temperatura media do periodo todo
+colnames(df)
+df$temp_media_total = (df$lstd_i + df$lstd_ii + df$lstd_iii)/3
+
+#Normalizacao
+#sorteia quem escolhe
+pptmax = max(df$ppt_total)
+pptmin = min(df$ppt_total)
+
+pptnorm = (df$ppt_total - pptmin)/(pptmax - pptmin)
+par(mfrow=c(1,2))
+plot(density(pptnorm))
+plot(density(df$ppt_total))
+par(mfrow=c(1,1))
+
+#funcao para normalizacao
+normalizar = function(x){
+  norm_x = (x - min(x))/(max(x)-min(x))
+  return(norm_x)
+}
+
+#apply no df todo
+norm_df = apply(df, 2, normalizar)
+norm_df = as.data.frame(norm_df)
+
+#verificar que os graficos no comeco nao mudam
+
+#separar treino e teste
+dim(norm_df)
+set.seed(42)
+test_rows = sample(nrow(df),0.25*nrow(df))
+
+test = df[test_rows,]
+train = df[-test_rows,]
+
+test_norm = norm_df[test_rows,]
+train_norm = norm_df[-test_rows]
+colnames(train)
+
+#modelo
+model = lm(tch~.,train_norm %>% select(-ppt_total))
+model$coefficients
+
+preds = predict(model,test_norm)
+preds 
+#desnormaliza
+preds = preds * (max(test$tch)-min(test$tch)) + min(test$tch)
+preds
+
+calc_mae(test$tch,preds)
+
+summary(model)
+sort((model$coefficients))
+
+
+#funcao para calcular o mae
+calc_mae = function(real,preds){
+  mean(abs(real - preds))
+}
+
+#loop para variar o grau
+#o que podemos variar?
+# grau do polinomio
+graus = c(2,3,4)
+mae_varia_grau = numeric()
+
+for(grau in graus){
+  mod = lm(tch~poly(estagio,grau),train)
+  preds = predict(mod,test)
+  mae_grau = calc_mae(test$tch,train)
+  mae_varia_grau = c(mae_varia_grau,mae_grau)
+}
+
+
+
+# features na formula
+# dataframe com diferentes graus e formulas
+formulas = c('tch~estagio','tch~estagio+ppt_i')
+
+
+
+
+#features numericos ainda
+
+
--- 03_linear_regression/pre/code/pre_aula03_solved.R
+++ 03_linear_regression/pre/code/pre_aula03_solved.R
@@ -1,12 +1,14 @@
 # Carregue o conjunto de dados
 # Lembre-se de definir o diretorio corretamente
 RA=123456
-df = read.csv('../pre/data/fa084_cana_estagio.csv')
+library(dplyr)
 
+df = read.csv('~/repos/fa084/03_linear_regression/pre/data/fa084_cana_estagio.csv')
+df = df %>% group_by(Estagio) %>% summarise(TCH = mean(TCH)) 
 #divida em treino e teste:
 set.seed(RA)
 rows_train = sample(nrow(df), 0.75*nrow(df))
-train = df[rows_train,]
+train = df#[rows_train,]
 test = df[-rows_train,]
 
 #Crie as variaveis a e b, que representam o grau do polinomio a ser treinado em cada caso
@@ -21,16 +23,27 @@ b = sample(c(4,5),1)
 #treine os dois modelos com a funcao lm() no conjunto de treino.
 #Dica: A formula é TCH~poly(Estagio,grau) onde grau é o grau do polinomio.
 #Devemos ter um modelo com grau a e outro com grau b
-modelo_grau_a = lm(TCH~poly(Estagio,a),data = df)
+modelo_grau_a = lm(TCH~poly(Estagio,1),data = train)
+modelo_grau_b = lm(TCH~poly(Estagio,3),data = train)
+summary(modelo_grau_a)
+modelo_grau_a$coefficients
+modelo_grau_b$coefficients
 
-modelo_grau_b = lm(TCH~poly(Estagio,b),data = df)
+
+predicted_a <- predict(modelo_grau_a,train)
+predicted_b <- predict(modelo_grau_b,train)
+
+plot(df$Estagio,df$TCH)
+points(train$Estagio,predicted_a,col='green',lwd=3)
+points(train$Estagio,predicted_b,col='red',lwd=3)
+
 
 
 #Faca a predicao no conjunto de teste usando o modelo a e o modelo b.
 #Use a funcao predict("modelo para fazer a previsao", newdata = "conjunto de dados de teste").
 pred_a = predict(modelo_grau_a,newdata = test)
 pred_b = predict(modelo_grau_b,newdata = test)
-  
+
 #Calcule o erro  medio absoluto (mae) em relacao a TCH do conjunto de teste para cada modelo.
 mean(abs(pred_a - test$TCH))
 mean(abs(pred_b - test$TCH))
@@ -39,3 +52,38 @@ mean(abs(pred_b - test$TCH))
   
 
 #Faca o grafico de TCH (eixo y) vs Estagio (eixo x) usando plot().
+
+
+
+# OUTRO EXEMPLO -----------------------------------------------------------
+
+p = 0.5
+q = seq(0,100,1)
+y = p*q
+plot(q,y,type='l',col='red',main='Linear relationship')
+
+
+
+y = -450 + p*(q-10)^3
+plot(q,y,type='l',col='navy',main='Nonlinear relationship',lwd=3)
+
+set.seed(20)
+q = seq(from=0, to=20, by=0.1)
+y = 500 + 0.4*(q-10)^3
+noise = rnorm(length(q), mean=10, sd=80)
+noisy.y = y + noise
+
+
+plot(q,noisy.y,col='blue',xlab='q',main='Observed data')
+lines(q,y,col='red',lwd=3)
+
+
+model = lm(noisy.y ~ poly(q,3))
+model1 = lm(noisy.y ~ q)
+summary(model)
+
+pred3 = predict(model)
+pred1 = predict(model1)
+
+lines(q,pred1,col='green',lwd=3)
+lines(q,pred3,col='black',lwd=3)
