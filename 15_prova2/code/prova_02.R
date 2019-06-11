72db4d0b7bfe2423be290a52cf91bd5216632573
mferrac <matheus.ferraciolli@gmail.com>
2019-06-11 11:46:03 -0300

    p2 ready for review

--- 15_prova2/code/prova_02.R
+++ 15_prova2/code/prova_02.R
@@ -15,35 +15,21 @@ normalize = function(col){
 data(PimaIndiansDiabetes)
 df = PimaIndiansDiabetes
 set.seed(2019)
-df = read_csv('../data/cardio_data.csv') %>% sample_n(1000)
+df = read_csv('../data/cardio.csv')
 
-# id  =	Patient ID
-# age =	Age in days
-# gender =	Male or female
-# age = Age in days
-# height =	Height in cm
-# weight =	Weight in kg
-# ap_hi =	Arterial Pressure (high)
-# ap_lo =	Arterial Pressure (low)
-# cholesterol =	Cholesterol Level
-# gluc =	Glucose Level
-# smoke =	Smoking or not
-# alco =	Drinks alco or not
-# active =	Practices physical acitivity at least 3 times a week
-# cardio =	Has cardio probem or not
 
-#Questão 1 - Exploração do conjunto de dados
+#Questao 1 - Exploracao do conjunto de dados -----------------------------------
 
 #Considere que vamos prever se um paciente tem ou nao problema cardiaco.
 
 #A) Esta e uma tarefa de classificacao ou regressao?
 
 #B) Quantos atributos preditores vamos usar?
-
+glimpse(df)
 #C) Quantos pacientes com e sem problemas cardiacos temos no dataset?
 
-#D) 1. Quantos homens e mulheres existem no dataset?Os gêneros estão divididos 
-#em 0 e 1, assuma que, em média, mulheres são mais leves e homens são mais altos,
+#D) 1. Quantos homens e mulheres existem no dataset? Os gêneros estao divididos 
+#em 0 e 1, assuma que, em media, mulheres sao mais leves e homens sao mais altos,
 #e verifique qual gênero corresponde a quais números.
 
 #E)Crie uma feature nova `bmi` que contem o indice de massa corporal, dado pela 
@@ -53,26 +39,46 @@ df = read_csv('../data/cardio_data.csv') %>% sample_n(1000)
 
 
 #1. A mediana do bmi esta num intervalo considerado normal (18.5-25)
-#2. O bmi para mulheres é, em média, maior que o de homens
-#3. Pessoas saudáveis (`cardio == 0`), tem, em média, bmi mais alto do que pessoas com doença (`cardio == 1`)
-#4. Para homens saudáveis, e que não consomem álcool, o bmi médio é mais proximo do normal do que para mulheres saudáveis que não consomem álcool
+#2. O bmi para mulheres e, em media, maior que o de homens
+#3. Pessoas saudáveis (`cardio == 0`), tem, em media, bmi mais alto do que pessoas com doenca (`cardio == 1`)
+#4. Para homens saudáveis, e que nao consomem álcool, o bmi medio e mais proximo do normal do que para mulheres saudáveis que nao consomem álcool
 
-#Questão 2 - Preparacao do conjunto de dados
-#A) Exclua coluna(s) que não será(ão) Considere inadequadas para modelagem.
+
+#Questao 2 - Preparacao do conjunto de dados------------------------------------
+#A) Exclua a coluna bmi (obrigatoria) e outra(s) coluna(s) que nao considere 
+# adequadas para modelagem.
 # Justifique brevemente o porquê.
-# Em seguida, normalize todas as colunas restantespara terem valores entre 0 e 1.
+# Em seguida, normalize todas as colunas restantes para terem valores entre 0 e 1.
 normdf = df %>% select(-id)
 normdf = df %>% apply(.,2,normalize) %>% as.data.frame()
 
 #B) Converta a coluna cardio para factor
 normdf = mutate(normdf,cardio=as.factor(cardio))
 
-#Nas questões 3 e 4 vamos comparar o desempenho de redes neurais e SVM para 
+#Na questão 3 vamos comparar o desempenho de redes neurais e SVM para 
 #classificar os pacientes em com ou sem problemas cardiacos.
 
-#Questão 3 - Neural Networks vs SVM
+#Questao 3 - Redes neurais -----------------------------------------------------
 
-#A) Crie uma task de treino (70%) e de teste. Use os procedimentos de "agora é sua vez",
+#A) Crie uma funcao sigmoide que tem como entrada um valor x e saída o valor da função sigmoide
+sigmoid = function(x){
+  1/(1 +exp(-x))
+}
+#B) Crie um vetor detamanho 6 com os digitos do seu RA em cada indice.
+ra_vec = c(1,1,8,1,0,0)
+  
+#C) Use o vetor w abaixo como pesos e o valor de b como vies.
+w = c(0.5,0.8,-0.3,-0.6,-1,1)
+b = 2
+
+#Calcule a saida de um neuronio que tem como entrada ra_vec, pesos w  e bias b, 
+# com a funcao de ativacao sigmoide. Quais os valores maximos e minimos da funcao?
+sigmoid(sum(ra_vec*w)+b)
+
+
+#Questao 4 - Neural Networks vs SVM --------------------------------------------
+
+#A) Crie uma task de treino (70%) e de teste. Use os procedimentos de "agora e sua vez",
 #da aula 11(11_mlr_svm_base.html, baixe o arquivo e abra no navegador).
 #Complete as lacunas abaixo. Crie mais variaveis se julgar adequado.
 task = makeClassifTask(data = normdf, target = 'cardio')
@@ -89,7 +95,16 @@ tsk_test = subsetTask(task, holdout$test.inds[[1]])
 svm_learner = makeLearner("classif.svm")
 nn_learner = makeLearner('classif.nnet')
 
-#C) Defina os parametros a serem ajustados
+#C) Defina os parametros a serem ajustados.
+#SVM:
+#Custo é um hiperparametro numerico que deve variar entre 0.1 e 1.
+#Gamma é um hiperparametro numerico que deve variar entre 0.1 e 2.
+
+#NN:
+#Size é um hiperparametro inteiro que deve variar de 2 a 5.
+
+
+
 params_svm = makeParamSet(makeNumericParam("cost",lower = 0.1,
                                            upper = 1),
                           makeNumericParam("gamma", lower = 0.1,
@@ -97,6 +112,7 @@ params_svm = makeParamSet(makeNumericParam("cost",lower = 0.1,
 
 params_nn = makeParamSet(makeIntegerParam("size",lower = 2,
                                          upper = 5))
+#D) Escolha um dos hiperparametros da SVM e explique seu significado
 
 #E) Defina o controle: RandomSearch com 50 iteracoes
 tune_control = makeTuneControlRandom(maxit=50)
@@ -114,7 +130,7 @@ set.seed(2019)
 tr_nn = tuneParams(nn_learner,tsk_train,cv5,list(acc),params_nn,tune_control)
 t1 = Sys.time()
 
-#G) Quais os melhores parâmetros de cada técnica?
+#G) Quais os melhores parâmetros de cada tecnica?
 tr_svm$x
 tr_nn$x
 
@@ -130,13 +146,17 @@ tuned_nn = train(tuned_nn, tsk_train)
 preds_svm = predict(tuned_svm,tsk_test)
 preds_nn = predict(tuned_nn,tsk_test)
 
-#K) Considere a premissa:
-#"É melhor dizer que o paciente tem doenca cardiaca e o mesmo não ter do que 
-# dizer que ele não tem e, na verdade, ter."
+#K) Preencha as matrizes de confusao de cada modelo. Use calculateConfusionMatrix()
+#nas predicoes acima.
+calculateConfusionMatrix(preds_svm)
+
+#Considere a premissa:
+#"e melhor dizer que o paciente tem doenca cardiaca e o mesmo nao ter do que 
+# dizer que ele nao tem e, na verdade, ter."
 #Qual modelo seria melhor nesse caso? Justifique com suas palavras.
 
 
-#Bonus:
+#Questao Bonus ----------------------------------------------------------------
 roc_svm = calculateROCMeasures(preds_svm)
 roc_nn = calculateROCMeasures(preds_nn)
 roc_svm
@@ -153,30 +173,40 @@ svm_results = roc_svm$measures %>% unlist()
 #Leia a documentacao da funcoes envolvidas, se necessario.
 compare_results = data.frame(nn_results,svm_results)
 colnames(compare_results) = c('nn','svm')
-compare_results = compare_results %>% rownames_to_column('measure') %>%
-  write_csv('../data/compare_results.csv')
+compare_results = compare_results %>% rownames_to_column('measure') 
 
-#Se nao conseguir construi o dataframe, importe o arquivo.
+#Questao 5: ggplot --------------------------------------------------------------
+#Independente de conseguir construir o dataframe, importe o arquivo compare_results.csv.
 #As medidas do arquivo nao necessariamente refletem as encontradas acima.
+#Use o compare_results como entrada do pipe abaixo para produzir o gráfico.
 
-#Questão 4: Use o compare_results como entrada do pipe abaixo para produzir o gráfico.
 #A) Explique o que a funcao "gather" faz, na linha abaixo.
 compare_results %>% gather("model","value",-measure) %>% 
-  ggplot(aes(x=measure,y=value,group=model,fill=model)) + 
+  ggplot(aes(x=measure,y=value,fill=model)) + 
   geom_col(position='dodge')
-#B) Mude nomes dos eixos x e y para "Measure" e "Value" e adicone o título "NN vs SVM
+#B) Mude nomes dos eixos x e y para "Measures" e "Value",respectivamente. 
+# Adicone o titulo "NN vs SVM"
+# Salve o novo plot na variavel "renamed_plot".
 renamed_plot = compare_results %>% gather("model","value",-measure) %>% 
   ggplot(aes(x=measure,y=value,group=model,fill=model)) + 
-  geom_col(position='dodge')
-  
-#C) Salve o gráfico na mesma pasta do script
+  geom_col(position = 'dodge') +
+  labs(x = "Measures",y="Value",title='NN vs SVM')
 renamed_plot
+  
+#C) Salve o gráfico (renamed_plot) na pasta 'data' com a extensão .jpg
+ggsave('../data/questao4_C.jpg',renamed_plot)
+
 
 #D) O que podemos fazer para diminuir a diferenca de proporcao entre as barras pequenas
 # e as grandes?
+renamed_plot + coord_trans(y='sqrt')
 
-# Questao 5
-#A) use os dados de USArrests (ja vem carregado no R).
+# Questao 6
+#A) Clusterize os dados de USArrests (ja vem carregado no R). Use a rotina vista em aula
+# para escolher o melhor numero de clusters. Teste valores de k de 1 a 20.
+# Além do parametro k, no loop, use nstart=25 dentro de makeLearner para ter um resultado mais consciso.
+# Use seed 2019 antes do loop
+
 cluster_data = USArrests
 cluster_data
 # Use o comando abaixo para remover os row_names de cluster_data
@@ -193,7 +223,7 @@ for(k in k_to_test){
   wss = tr$learner.model$tot.withinss
   wssvec = c(wssvec,wss)
 }
-plot(k_to_test,wssvec)
+plot(k_to_test,wssvec,type='l')
 
 learner = makeLearner('cluster.kmeans',centers=3)
 tr = train(learner,cluster_task)
@@ -206,6 +236,3 @@ head(cluster_data)
 
 #C) Temos 3 novas observacoes arquivo find_cluster.csv. 
 # Use uma rotina concisa para predizer a qual cluster cada uma pertence.
-
-
-
